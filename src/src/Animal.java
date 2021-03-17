cc06921148ca12b2d227279de6ed24682c7b0bc1
Daedilous <55263986+Daedilous@users.noreply.github.com>
2021-03-17 10:36:05 +0000

    Update Animal.java
    tidied altered in case its used for inheritance.

--- src/src/Animal.java
+++ src/src/Animal.java
@@ -7,40 +7,29 @@
  *
  */
 public class Animal {
+	
 	private static int health = 0;
 	private static int food = 0;
 	private static int stamina = 0;
 	private static int mood = 0;
 	
 	public Animal(int food, int health, int stamina, int mood) {
-		Animal.health = health;
-		Animal.food = food;
-		Animal.stamina = stamina;
-		Animal.mood = mood;
+		This.health = health;
+		This.food = food;
+		This.stamina = stamina;
+		This.mood = mood;
 	}
-	public static int getHealth() {
-		return health;
-	}
-	public static int getFood() {
-		return food;
-	}
-	public static int getStamina() {
-		return stamina;
-	}
-	public static int getMood() {
-		return mood;
-	}
-	public void setHealth(int  h) {
-		health = h;
-	}
-	public void setFood(int  f) {
-		food = f;
-	}
-	public void setStamina(int  s) {
-		stamina = s;
-	}
-	public void setMood(int  m) {
-		mood = m;
-	}
+	
+	// animal getters
+	public static int getHealth() {return health;}
+	public static int getFood() {return food;}
+	public static int getStamina() {return stamina;}
+	public static int getMood() {return mood;}
+	
+	// animal setters
+	public void setHealth(int  h) {health = h;}
+	public void setFood(int  f) {food = f;}
+	public void setStamina(int  s) {stamina = s;}
+	public void setMood(int  m) {mood = m;}
 
 }
