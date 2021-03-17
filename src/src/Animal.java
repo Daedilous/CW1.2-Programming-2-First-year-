e8307a4b8fdb819b75c45f3876ba08d0a38f6b7e
Rajyzle <80822494+Rajyzle@users.noreply.github.com>
2021-03-17 11:32:23 +0000

    Update Animal.java

--- src/src/Animal.java
+++ src/src/Animal.java
@@ -1,3 +1,5 @@
+
+  
 /**
  * 
  */
@@ -12,24 +14,29 @@ public class Animal {
 	private static int food = 0;
 	private static int stamina = 0;
 	private static int mood = 0;
+	private static String name = "";
 	
-	public Animal(int food, int health, int stamina, int mood) {
-		This.health = health;
-		This.food = food;
-		This.stamina = stamina;
-		This.mood = mood;
+	public Animal(String name, int food, int health, int stamina, int mood) {
+		Animal.name = name;
+		Animal.health = health;
+		Animal.food = food;
+		Animal.stamina = stamina;
+		Animal.mood = mood;
 	}
 	
 	// animal getters
+	public static String getName() {return name;}
 	public static int getHealth() {return health;}
 	public static int getFood() {return food;}
 	public static int getStamina() {return stamina;}
 	public static int getMood() {return mood;}
 	
 	// animal setters
+	public void setName(String n) {name = n;}
 	public void setHealth(int  h) {health = h;}
 	public void setFood(int  f) {food = f;}
 	public void setStamina(int  s) {stamina = s;}
 	public void setMood(int  m) {mood = m;}
 
+}
 }
