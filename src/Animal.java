a56067dd84f619659a04daa69696fa69d3ee2dea
Rajyzle <80822494+Rajyzle@users.noreply.github.com>
2021-04-22 13:15:11 +0100

    Update Animal.java

--- src/Animal.java
+++ src/Animal.java
@@ -1,22 +1,42 @@
+  
 /**
- * @author Lahiru Rajamanthri
- *
+ * 
+ 	*@author Lahiru Rajamanthri
+	 * @author Harrison Bell
+	 * @author Jade Sumner
+	 * @author Luke Reading
+ * This class holds the stats for the pet. 
+ * if health gets to 0 the pet dies.
+ * if food is 0 pet cannot eat
+ * 
  */
 public class Animal {
-	
+	/*
+	 * class integer and string values to hold the Animal's parameters
+	 */
 	private String name = "";
 	private int health = 0;
 	private int food = 0;
 	private int stamina = 0;
 	private int mood = 0;
+	private int foodBucket = 0;
 	
-	
-	public Animal(String name, int food, int health, int stamina, int mood) {
+	public Animal(String name, int food, int health, int stamina, int mood, int foodBucket) {
+		/**
+		 * Constructor for Animal object
+		 * @param	name	Pet's name
+		 * @param 	food	Pet's food value
+		 * @param 	health 	Pet's health value
+		 * @param	stamina	Pet's stamina value
+		 * @param	mood	Pet's mood value
+		 * @param	foodBucket	Pet's food bucket value
+		 */
 		this.name = name;
 		this.health = health;
 		this.food = food;
 		this.stamina = stamina;
 		this.mood = mood;
+		this.foodBucket = foodBucket;
 	}
 	
 	// animal getters
@@ -25,6 +45,7 @@ public class Animal {
 	public int getFood() {return this.food;}
 	public int getStamina() {return this.stamina;}
 	public int getMood() {return this.mood;}
+	public int getFoodBucket() {return this.foodBucket;}
 	
 	// animal setters
 	public void setName(String n) {this.name = n;}
@@ -32,22 +53,5 @@ public class Animal {
 	public void setFood(int  f) {this.food = f;}
 	public void setStamina(int  s) {this.stamina = s;}
 	public void setMood(int  m) {this.mood = m;}
-	
-	// animal assessment
-	public String assessment() {
-		String assessment = "";
-		if(this.health <= 50) {
-			assessment += "I need care. ";
-		}else if(this.food <= 50) {
-			assessment += "I need food. ";
-		}else if(this.stamina <= 50) {
-			assessment += "I need rest. ";
-		}else if(this.mood <= 50) {
-			assessment += "I want to play";
-		}else {
-			assessment = "Your pet is healthy";
-		}
-		return assessment;
-	}
-
+	public void setFoodBucket(int fb) {this.foodBucket = fb;}
 }
